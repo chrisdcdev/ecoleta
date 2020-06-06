bb38bf79f3a84f38f4165a3748ec87749d84b399
Christian Castro <alvescastro00cca13@gmail.com>
2020-06-05 23:12:33 -0300

    Finished frontend

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState, ChangeEvent} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link ,useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import logo from '../../assets/logo.svg';
@@ -10,74 +10,130 @@ import './styles.css';
 import axios from 'axios';
 
 
-interface Item{
+interface Item {
     id: number;
     title: string;
-    image:string;
+    image: string;
 }
 
-interface IBGEUFResponse{
-    sigla:string;
+interface IBGEUFResponse {
+    sigla: string;
 }
 
-interface IBGECityResponse{
+interface IBGECityResponse {
     nome: string;
 }
 
 const CreatePoint = () => {
     const [items, setItems] = useState<Item[]>([]);
     const [ufs, setUfs] = useState<string[]>([]);
-    const [cities, setCities] = useState<string[]>([]); 
-
+    const [cities, setCities] = useState<string[]>([]);
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
-    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [initialPosition, setInicialPosition] = useState<[number, number]>([0, 0]);
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        whatsapp: '',
+    })
 
 
-    useEffect(()=>{
-        api.get('items').then(response =>{
+    useEffect(() => {
+        api.get('items').then(response => {
             setItems(response.data)
-    
+
         });
     }, []);
 
 
-useEffect(()=>{
-    axios.
-    get<IBGEUFResponse[]>
-    ('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-            .then(response =>{
-            const ufInitials = response.data.map(uf=>uf.sigla);
-            setUfs(ufInitials);
+    useEffect(() => {
+        axios.
+            get<IBGEUFResponse[]>
+            ('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then(response => {
+                const ufInitials = response.data.map(uf => uf.sigla);
+                setUfs(ufInitials);
             })
-}, []);
+    }, []);
 
-useEffect(()=>{
-    axios.
-    get<IBGECityResponse[]>
-    (`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
-            .then(response =>{
-            const cityNames = response.data.map(city=>city.nome);
-            setCities(cityNames);
+    useEffect(() => {
+        axios.
+            get<IBGECityResponse[]>
+            (`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+            .then(response => {
+                const cityNames = response.data.map(city => city.nome);
+                setCities(cityNames);
             })
-}, [selectedUf]);
+    }, [selectedUf]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            setInicialPosition([latitude, longitude]);
+        })
+    })
 
 
-    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>){
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
         const uf = event.target.value;
         setSelectedUf(uf);
     }
 
-    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>){
+    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
         const city = event.target.value;
         setSelectedCity(city);
     }
 
-    function handleMapClick(event: LeafletMouseEvent){
+    function handleMapClick(event: LeafletMouseEvent) {
         setSelectedPosition([
             event.latlng.lat,
             event.latlng.lng,
         ])
+    }
+
+    function handleImputChange(event: ChangeEvent<HTMLInputElement>){
+        const {name, value} = event.target;
+
+        setFormData({ ...formData, [name]: value })
+    }
+
+    function handleSelectItem(id:number){
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+        
+        if(alreadySelected >= 0){
+            const filteredItems = selectedItems.filter(item => item !== id);
+            setSelectedItems(filteredItems);
+        }
+        else{
+            setSelectedItems([...selectedItems, id])
+        }
+    }
+
+    async function handleSubmit( event: FormEvent){
+        event.preventDefault();
+
+        const {name, email, whatsapp} = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems; 
+
+        const data = {
+            name, 
+            email,
+            whatsapp,
+            uf,
+            city,
+            latitude,
+            longitude,
+            items
+        };
+       await api.post('points', data);
+       alert('Ponto de coleta criado!');
+       history.push('/')
     }
 
 
@@ -92,7 +148,7 @@ useEffect(()=>{
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do<br /> ponto de coleta</h1>
 
                 <fieldset>
@@ -107,6 +163,7 @@ useEffect(()=>{
                         type="text"
                         name="name"
                         id="name"
+                        onChange={handleImputChange}
                     />
                 </div>
 
@@ -118,6 +175,8 @@ useEffect(()=>{
                             type="email"
                             name="email"
                             id="email"
+                            onChange={handleImputChange}
+
                         />
                     </div>
                     <div className="field">
@@ -126,6 +185,8 @@ useEffect(()=>{
                             type="text"
                             name="whatsapp"
                             id="whatsapp"
+                            onChange={handleImputChange}
+
                         />
                     </div>
 
@@ -137,30 +198,30 @@ useEffect(()=>{
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
-                    <Map center={[-4.4873657, -38.5982356]} zoom={15} onclick={handleMapClick}>
+                    <Map center={initialPosition} zoom={15} onclick={handleMapClick}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
 
-                        <Marker position={selectedPosition}/>
+                        <Marker position={selectedPosition} />
                     </Map>
 
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estafo</label>
-                            <select 
-                                name="uf" 
-                                id="uf" 
-                                value={selectedUf} 
+                            <select
+                                name="uf"
+                                id="uf"
+                                value={selectedUf}
                                 onChange={handleSelectUf}
                             >
 
                                 <option value="0">Selecione uma UF</option>
 
-                                {ufs.map(uf=> (
-                                    <option 
-                                        key={uf} 
+                                {ufs.map(uf => (
+                                    <option
+                                        key={uf}
                                         value={uf}> {uf} </option>
 
                                 ))}
@@ -169,18 +230,18 @@ useEffect(()=>{
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select 
-                                name="city" 
+                            <select
+                                name="city"
                                 id="city"
                                 value={selectedCity}
                                 onChange={handleSelectCity}
                             >
                                 <option value="0">Selecione uma cidade</option>
 
-                                {cities.map(city=>(
-                                    <option 
-                                        key={city} 
-                                        value={city}> {city} </option> 
+                                {cities.map(city => (
+                                    <option
+                                        key={city}
+                                        value={city}> {city} </option>
                                 ))}
                             </select>
                         </div>
@@ -195,11 +256,16 @@ useEffect(()=>{
 
                     <ul className="items-grid">
 
-                        {items.map( item => (
-                            <li key={item.id}>
-                               <img src={item.image} alt={item.title} />
+                        {items.map(item => (
+                            <li 
+                                key={item.id} 
+                                onClick={ () => handleSelectItem(item.id)}
+                                className={selectedItems.includes(item.id) ? 'selected' : ''}
+                                
+                            >
+                                <img src={item.image} alt={item.title} />
                                 <span>{item.title}</span>
-                           </li>
+                            </li>
                         ))}
 
                     </ul>
